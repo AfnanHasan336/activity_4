db3d1d5d17c652bf7bf5c4f76128eaf25d34e6f0
AfnanHasan336 <anh3100@g.rit.edu>
2024-11-26 20:36:02 +0400

    committing main 2 files

---
+++ test_polygon.py
@@ -0,0 +1,80 @@
+from polygon import Polygon
+from pytest import approx
+
+def test_polygon_initialization():
+    '''
+    This function tests if the attributes of __init__() function are initialized porperly
+    '''
+    poly = Polygon("Pentagon", [5,5,5,5,5])
+
+    assert poly.get_name() == "Pentagon"
+    assert poly.get_sides() == [5,5,5,5,5]  # to check if name and sides are working correctly in Polygon class
+
+def test_get_name():
+    '''
+    This function checks if the function get_name() returns the name correctly
+    '''
+    poly = Polygon("Triangle", [3,3,3])
+
+    assert poly.get_name() == "Triangle"  # checks if value of name is returned properly
+
+def test_set_name():
+    '''
+    This function checks if the set_name() function sets new value of name correctly
+    '''
+    poly = Polygon("Hexagon", [6,6,6,6,6,6])
+    poly.set_name("Rectangle")
+
+    assert poly.get_name() == "Rectangle"  # checks if value of name is changed properly
+
+def test_get_sides():
+    '''
+    This function checks if the get_sides() function returns the value sides correctly
+    '''
+    poly = Polygon("Heptagon", [7,7,7,7,7,7,7])
+
+    assert poly.get_sides() == [7,7,7,7,7,7,7]  # checks if value of sides is returned properly
+
+def test_set_sides():
+    '''
+    This function checks if the set_sides() function sets the new value of sides correctly
+    '''
+    poly = Polygon("Octagon", [8,8,8,8,8,8,8,8])
+    poly.set_sides([7,7,7,7,7,7,7])
+
+    assert poly.get_sides() == [7,7,7,7,7,7,7]  # checks if the value of sides is changed properly
+
+def test_equality():
+    '''
+    This function checks if equality operator is correctly defined
+    '''
+    poly1 = Polygon("Rectangle", [4,4,4,4])
+    poly2 = Polygon("Rectangle", [4,4,4,4])
+
+    assert poly1 == poly2  # checking betweeen 2 objects if they are equal
+
+def test_inequality():
+    '''
+    This function checks if inequality operator is correctly defined
+    '''
+    poly1 = Polygon("Rectangle", [4,4,4,4])
+    poly2 = Polygon("Triangle", [3,3,3])
+
+    assert poly1 != poly2  # checking between 2 objects if they are not equal
+
+def test_str():
+    '''
+    This function checks if the string representation gives correct string
+    '''
+    poly = Polygon("Triangle", [3,3,3])
+    should_print = "Triangle with sides: [3, 3, 3]" 
+    
+    assert str(poly) == should_print  # checks if string of object poly is giving same output 
+
+def test_circumfrence():
+    '''
+    This function checks if the circumference function returns the correct sum of sides
+    '''
+    poly = Polygon("Triangle", [3,3,3])
+    
+    assert poly.calculate_circumference() == approx(9.0)  # approx function from pytest module does floating-point comparison 
